f9935c437a6bff19097a95aed6c4d506f75e9d77
Nick Siderakis <nick@learnvest.com>
2013-10-12 13:06:58 -0400

    explicitly use global ExecutionContext

--- src/main/scala/FuturaeServlet.scala
+++ src/main/scala/FuturaeServlet.scala
@@ -1,14 +1,12 @@
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
 import scala.concurrent._
-import ExecutionContext.Implicits.global
-
 
 class FuturaeServlet extends HttpServlet {
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse) = {
 
     val f: Future[List[String]] = future {
       List("a", "b", "c")
-    }
+    }(ExecutionContext.global)
 
     f.map(_.foreach(resp.getOutputStream.println))
   }
